2958a502cd7c86a3f80e689bf81044084939f301
shinji1229 <fk.shinji1229@gmail.com>
2017-05-04 20:11:55 +0900

    sign_up時にnameカラムをparamsで取得できるようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
