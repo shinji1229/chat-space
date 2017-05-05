6897ace3512e97b09dc407f1d9aa08b220f4c31a
shinji1229 <fk.shinji1229@gmail.com>
2017-05-05 12:57:32 +0900

    サインアウト後のpathをサインイン画面に指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def after_sign_out_path_for(resource)
+    '/users/sign_in'
+  end
+
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.sanitize(:account_update, keys: [:name])
