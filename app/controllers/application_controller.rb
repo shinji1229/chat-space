d9548a60f252357a71ed7607869e45680258ebc0
shinji1229 <fk.shinji1229@gmail.com>
2017-05-05 14:17:19 +0900

    authenticate_user!をapplication_controllerに追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def after_sign_out_path_for(resource)
-    '/users/sign_in'
-  end
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.sanitize(:account_update, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
 end
