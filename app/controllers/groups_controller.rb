fcc617420a4f55f319abeead3377cb78b502fd7c
shinji1229 <fk.shinji1229@gmail.com>
2017-05-09 12:32:44 +0900

    new.html.hamlをform_tagで書き換えた

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,19 @@
 class GroupsController < ApplicationController
+
+  def new
+    @groups = Group.new
+  end
+
+  def create
+    @groups = Group.create(name: group_params[:text])
+  end
+
+  def edit
+    @groups = Group.find()
+  end
+
+  private
+  def group_params
+    params.permit(:text)
+  end
 end
