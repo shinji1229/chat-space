b5add93a40bc845bd2ae760f66c0a8b19e09f9ca
shinji1229 <fk.shinji1229@gmail.com>
2017-05-12 20:59:27 +0900

    送信機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,21 @@ class MessagesController < ApplicationController
     @groups = current_user.groups.order("created_at DESC")
     @messages = @group.messages
     @users = @group.users
+    @message = Message.new
   end
 
+  def create
+    @message = current_user.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path, notice:'メッセージを作成しました'
+    else
+      flash.now[:alert] = 'メッセージを入力してください'
+      render :new
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id])
+  end
 end
