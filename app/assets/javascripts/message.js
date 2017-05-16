d0d4ef81af7c05fb8ee42d03490880913a178514
shinji1229 <fk.shinji1229@gmail.com>
2017-05-16 18:07:12 +0900

    APIを作成

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,35 @@
+$(function() {
+  function buildHTML(message) {
+    var html = {};
+    html['html_user'] = $('.chat-main__body_name').html('message.user.name');
+    html['html_date'] = $('chat-main__body_date').html('message.created_at');
+    html['html_body'] = $('.chat-main__body_message').html('message.body');
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var textField = $('#message_body');
+    var message = textField.val();
+    $.ajax({
+      type: 'POST',
+      url: './messages',
+      data: {
+        message: {
+          body: message
+        }
+      },
+      dataType: 'json'
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $.each(html,function(i,val){
+        $('.chat-main__body').append(val);
+      });
+      textField.val('');
+    })
+    .fail(function() {
+      alert('メッセージを送信できませんでした');
+    });
+  });
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,11 @@ class MessagesController < ApplicationController
   def create
     @message = current_user.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path, notice:'メッセージを作成しました'
+      respond_to do |format|
+      format.html {  redirect_to group_messages_path, notice:'メッセージを作成しました' }
+      format.json { render json: @message}
+      end
+
     else
       flash.now[:alert] = 'メッセージを入力してください'
       render :index
