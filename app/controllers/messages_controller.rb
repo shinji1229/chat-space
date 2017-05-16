c00002631ccdf65ae3f5b37134da05230ac33725
shinji1229 <fk.shinji1229@gmail.com>
2017-05-16 12:27:05 +0900

    軽微な修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,9 +10,11 @@ class MessagesController < ApplicationController
     @message = current_user.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path, notice:'メッセージを作成しました'
+
     else
       flash.now[:alert] = 'メッセージを入力してください'
       render :index
+
     end
   end
 
